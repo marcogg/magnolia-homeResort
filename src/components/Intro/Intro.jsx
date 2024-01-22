3e8007b29e7c01abc063fe6dd50c1fa80f729b8d
Marco Garcia <marcogarcia.gon@gmail.com>
2024-01-22 13:38:57 -0600

    bullets in home

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -10,19 +10,30 @@ const Intro = () => {
           <div className="detail-box">
             <div className="heading_container">
               <h2>
-                About Studio
+                Vive cada día como si fueran vacaciones <span className="light-300">En Lomas de Angelópolis</span>
               </h2>
               <p>
-                It is a long established fact that a reader will be distracted by the readable content of a page
+              Lomas de Angelópolis es un proyecto urbanístico de clase mundial, ubicado en la ciudad de Puebla, sobresale por su arquitectura innovadora, excelente diseño vial y su modernidad, cuanta con equipamientos y servicios como:
               </p>
-              <a href>
-                Read More
-              </a>
             </div>
           </div>
         </div>
+              <div className="row align-items-center">
+              <div>
+                <div><span className="material-symbols-outlined">school</span> Escuelas </div>
+                <div><span className="material-symbols-outlined">local_mall</span> Centros comerciales </div>
+                <div><span className="material-symbols-outlined">medication</span> Hospitales </div>
+                <div><span className="material-symbols-outlined">work</span> Corporativos </div>
+                <div><span className="material-symbols-outlined">bedroom_parent</span> Hoteles </div>
+                <div><span className="material-symbols-outlined">nature</span> 30 parques </div>
+                <div><span className="material-symbols-outlined">forest</span> 100 hectáreas de áreas verdes </div>
+                <div><span className="material-symbols-outlined">pedal_bike</span> 17 km de ciclopista </div>
+                <div><span className="material-symbols-outlined">sports_soccer</span> 30 canchas deportivas </div>
+                <div><span className="material-symbols-outlined">brush</span>Espacios artísticos</div>
+              </div>
+              </div>
+        </div>
       </div>
-    </div>
     <div className="img-box">
       <div className="play_btn">
         <a href="#">
