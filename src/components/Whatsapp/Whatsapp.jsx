227f3b2dab099f369b2a3252401b3562784e8c78
Marco Garcia <marcogarcia.gon@gmail.com>
2024-03-23 21:03:04 -0600

    feature: Thank you page

--- src/components/Whatsapp/Whatsapp.jsx
+++ src/components/Whatsapp/Whatsapp.jsx
@@ -4,8 +4,11 @@ const Whatsapp = () => {
   return (
     <>
       <div style={{ backgroundColor: '#02a047', width: 70, height: 70, borderRadius: '50%', position: 'fixed', bottom: 50, right: 5, display: 'block' }}>
-        <div style={{ margin: 'auto', display: 'block', position: 'relative', left: '18%', top: '13%', zindex: 50000 }}>
-          <Link to='https://api.whatsapp.com/send?phone=522221209242&text=%C2%A1Hola!%20Quiero%20m%C3%A1s%20informaci%C3%B3n%20del%20desarrollo%20Magnolia'>
+        <div style={{ margin: 'auto', display: 'block', position: 'relative', left: '18%', top: '13%', zIndex: 50000 }}>
+          <Link onClick={(e) => {
+            e.preventDefault();
+            window.location = 'https://api.whatsapp.com/send?phone=522221209242&text=%C2%A1Hola!%20Quiero%20m%C3%A1s%20informaci%C3%B3n%20del%20desarrollo%20Magnolia'
+          }}>
             <img src='../whatsapp-outline-01.png' alt='Whatsapp' style={{ width: '47px !important' }} />
           </Link>
         </div>
--- src/main.jsx
+++ src/main.jsx
@@ -2,43 +2,48 @@
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import Amenidades from './Pages/Amenidades'
-import {Contacto} from './Pages/Contacto'
+import { Contacto } from './Pages/Contacto'
 import Desarrollo from './Pages/Desarrollo'
 import Galeria from './Pages/Galeria'
 import './app.scss'
 import { createBrowserRouter, Navigate, RouterProvider } from 'react-router-dom'
+import Gracias from './Pages/Gracias.jsx'
 
 const router = createBrowserRouter(
   [
-  {
-    path: '/',
-    element: <App />
-  },
-  {
-    path: '/amenidades',
-    element: <Amenidades />
-  },
-  {
-    path: '/contacto',
-    element: <Contacto />
-  },
-  {
-    path: '/desarrollo',
-    element: <Desarrollo />
-  },
-  {
-    path: '/galeria',
-    element: <Galeria />
-  },{
-    path: '*',
-    element: <Navigate to='/' />
-  }
-
-]
+    {
+      path: '/',
+      element: <App />
+    },
+    {
+      path: '/amenidades',
+      element: <Amenidades />
+    },
+    {
+      path: '/contacto',
+      element: <Contacto />
+    },
+    {
+      path: '/desarrollo',
+      element: <Desarrollo />
+    },
+    {
+      path: '/galeria',
+      element: <Galeria />
+    },
+    {
+      path: '/gracias-por-contactarnos',
+      element: <Gracias />
+    },
+    {
+      path: '*',
+      element: <Navigate to='/' />
+    }
+  ]
 )
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 
-    <RouterProvider router={router} />
-    
+  <RouterProvider router={router} />
+
 )
